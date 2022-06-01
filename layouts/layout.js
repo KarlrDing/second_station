2e630840dcc8ba90329ce9bc4f811542288ec1fa
Karl Ding <86964458+KarlrDing@users.noreply.github.com>
2022-06-01 01:32:12 +0100

    Update layout.js

--- layouts/layout.js
+++ layouts/layout.js
@@ -42,7 +42,7 @@ const Layout = ({
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src={`https://gravatar.com/avatar/${emailHash}`}
+                  src={`https://s.gravatar.com/avatar/4c151f9590eb126daf5cbb18c7d9dd39?s=80`}
                   className="rounded-full"
                 />
                 <p className="ml-2 md:block">{BLOG.author}</p>
